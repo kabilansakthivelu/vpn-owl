02c8f559c940fdbe6139f5a17c4b7c4e5d892ea5
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2022-01-28 12:16:42 +0530

    Committing changes made for About component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
-  return <div><h1>About</h1></div>;
+  return (
+  <div className="aboutSection">
+  <h1 className="sectionTitle">About</h1>
+  <ul>
+  <li className="about"><b>VPN Owl</b> keeps your online activity safe, secure, and private - <b> for your eyes only!. </b> Trusted by 80+ million users worldwide for VPN <b>stability, speed, and security.</b></li>
+  <li className="about">We truly believe in 100% internet privacy, so the app also comes with a <b> Private Browser </b> for more privacy than the incognito mode.</li>
+  <li className="about">With VPN Owl, feel confident that your internet activity (home, public wifi, cellular) is protected, and anonymous. It guards your passwords, credit card, and bank accounts details (amongst others) from hackers and trackers. Plus, gives you <b> unrestricted access to global internet content.</b> Along with that, it lets you unblock popular apps like WhatsApp, TikTok, WeChat, Telegram, Roblox, and lots more.</li>
+  </ul>
+  </div>
+  );
 };
 
 export default About;
